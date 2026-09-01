68c560f04f2021fcbff10ea0f94f7e676d6f2da4
jcode <akandebabatundeh2@gmail.com>
2026-09-01 17:24:08 +0100

    chore: configure project for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,16 @@
-// import type { NextConfig } from "next";
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 const nextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Disable TypeScript errors during builds (use with caution)
+
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Customize Webpack configuration to only display errors in the build logs
-  webpack: (config, { isServer, dev }) => {
+
+  webpack: (config) => {
     config.stats = "errors-only";
     return config;
   },
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
